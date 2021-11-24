1c6897b8a4625b1a20f525dd40db8d210f4823dd
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2021-11-24 16:46:50 +0100

    deleted build folder

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Scroll = (props) => {
-	console.log(props);
 	return (
 		<div style={{ overflow: 'scroll', border: '3px solid black', height: '500px' }}>
 			{props.children}
